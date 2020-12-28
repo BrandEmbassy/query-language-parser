8bda3d6909856b112a22ae44442be4eef21a5055
Jan Vesely <janvesely@janvesely.net>
2020-12-28 16:28:26 +0100

    basic setup of tools (phpcs, phpstan, phpunit) in php library

---
+++ src/World.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\QueryLanguageParser;
+
+final class World
+{
+    public function sayHello(): string
+    {
+        return 'Hello world';
+    }
+}
