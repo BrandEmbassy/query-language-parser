fec692e052a73f037d7e0cdbdda7dc50a9f612ab
Dmitrii Diakonov <diakonov.dmitrii@gmail.com>
2023-07-25 09:45:51 +0200

    DE-57511: fixed PositiveIntegerValueParserCreator regex

--- src/Value/PositiveIntegerValueParserCreator.php
+++ src/Value/PositiveIntegerValueParserCreator.php
@@ -14,7 +14,7 @@ class PositiveIntegerValueParserCreator
 {
     use StaticClass;
 
-    private const POSITIVE_INTEGER_VALUE_REGEX = '#^([1-9]\d{0,8}|1\d{9}|20\d{8}|21[0-3]\d{7}|214[0-6]\d{6}|2147[0-3]\d{5}|21474[0-7]\d{4}|214748[0-2]\d{3}|2147483[0-5]\d{2}|21474836[0-3]\d|214748364[0-7])$#';
+    private const POSITIVE_INTEGER_VALUE_REGEX = '#^([1-9]\d{0,8}|1\d{9}|20\d{8}|21[0-3]\d{7}|214[0-6]\d{6}|2147[0-3]\d{5}|21474[0-7]\d{4}|214748[0-2]\d{3}|2147483[0-5]\d{2}|21474836[0-3]\d|214748364[0-7])#';
 
 
     /**
