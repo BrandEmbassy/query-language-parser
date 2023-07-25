4b268e444f57137c7e2684c4fc3474ddf7048a6b
Dmitrii Diakonov <diakonov.dmitrii@gmail.com>
2023-07-25 10:28:14 +0200

    DE-57511: fixed PositiveIntegerValueParserCreator regex

--- src/Value/PositiveIntegerValueParserCreator.php
+++ src/Value/PositiveIntegerValueParserCreator.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace BrandEmbassy\QueryLanguageParser\Value;
 
@@ -6,6 +6,7 @@ use Ferno\Loco\GrammarException;
 use Ferno\Loco\MonoParser;
 use Ferno\Loco\RegexParser;
 use Nette\StaticClass;
+use function trim;
 
 /**
  * @final
@@ -14,7 +15,7 @@ class PositiveIntegerValueParserCreator
 {
     use StaticClass;
 
-    private const POSITIVE_INTEGER_VALUE_REGEX = '#^([1-9]\d{0,8}|1\d{9}|20\d{8}|21[0-3]\d{7}|214[0-6]\d{6}|2147[0-3]\d{5}|21474[0-7]\d{4}|214748[0-2]\d{3}|2147483[0-5]\d{2}|21474836[0-3]\d|214748364[0-7])$#';
+    private const POSITIVE_INTEGER_VALUE_REGEX = '#^\s*([1-9]\d{0,8}|1\d{9}|20\d{8}|21[0-3]\d{7}|214[0-6]\d{6}|2147[0-3]\d{5}|21474[0-7]\d{4}|214748[0-2]\d{3}|2147483[0-5]\d{2}|21474836[0-3]\d|214748364[0-7])\s*$#';
 
 
     /**
@@ -24,7 +25,7 @@ class PositiveIntegerValueParserCreator
     {
         return new RegexParser(
             self::POSITIVE_INTEGER_VALUE_REGEX,
-            static fn($value): int => (int)$value,
+            static fn($value): int => (int)trim($value),
         );
     }
 }
